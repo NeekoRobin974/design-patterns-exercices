a10fea558dcdc8363cd0af2b180429eeae512053
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2026-03-10 15:58:32 +0100

    Factory

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,23 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config{
+    private static $instance;
+    private $settings = [];
+
+    private function __construct(){
+        $this->settings = require('../config/config.php');
+    }
+
+    public static function getInstance(): Config{
+        if (!self::$instance) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function get(){
+        return $this->settings['apiKey'];
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -5,3 +5,15 @@ require('../vendor/autoload.php');
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+use App\Config;
+
+$config1 = Config::getInstance();
+echo "API Key: " . $config1->get() . "<br>";
+
+$config2 = Config::getInstance();
+if ($config1 === $config2) {
+    echo "Instances identiques";
+} else {
+    echo "Instances différentes.";
+}
